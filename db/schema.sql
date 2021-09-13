e5a293ec363475b8f8090dd6c56ffc9f79bb496c
kellydhunter1 <kellydhunter1@gmail.com>
2021-09-13 16:23:26 -0400

    created database and employee table

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS employees;
+-- DROP TABLE IF EXISTS candidates;
+-- DROP TABLE IF EXISTS parties;
+-- DROP TABLE IF EXISTS voters;
+
+CREATE TABLE employees (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL, 
+  position VARCHAR(30) NOT NULL,
+  department_id INTEGER NOT NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO employees (first_name, last_name, position, department_id )
+VALUES
+  ('Kamryn', 'Gabrielle', 'talent', 1),
+  ('Graham', 'Hunter', 'assistant', 2);
