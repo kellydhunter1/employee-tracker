792ace0d7b90ee151de64c2b9b6f0af3c1d19caa
kellydhunter1 <kellydhunter1@gmail.com>
2021-09-13 16:44:45 -0400

    made query to database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,8 +7,23 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'B00tcamp1',
+      database: 'company'
+    },
+    console.log('Connected to the company database.')
+  );
 
-
+// database routes
+  db.query(`SELECT * FROM employees`, (err, rows) => {
+    console.log(rows);
+  });
   // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
